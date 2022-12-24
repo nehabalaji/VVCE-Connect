b03064603efbebfeafe48054c19f251fd2df67d6
SamB-12 <bedre.samarth@gmail.com>
2022-12-24 15:58:59 +0530

    design login page a basic login page with necessary fields

--- app/src/main/java/com/project/vvce_connect/ui/screens/registration/components/ButtonComponent.kt
+++ app/src/main/java/com/project/vvce_connect/ui/screens/registration/components/ButtonComponent.kt
@@ -1,12 +1,14 @@
 package com.project.vvce_connect.ui.screens.registration.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -21,6 +23,7 @@ import androidx.compose.ui.unit.dp
 fun ButtonComponent(text:String) {
     Button(
         onClick = { /*TODO*/ },
+        modifier = Modifier.fillMaxWidth(0.78f),
         colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.secondary)
     ) {
         Text(
