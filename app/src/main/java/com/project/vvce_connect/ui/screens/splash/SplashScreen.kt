a192e3be357e3844b0164597baf22c82b09e39b4
SamB-12 <bedre.samarth@gmail.com>
2022-12-27 10:30:36 +0530

    make changes to splash screen

--- app/src/main/java/com/project/vvce_connect/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/project/vvce_connect/ui/screens/splash/SplashScreen.kt
@@ -2,7 +2,6 @@ package com.project.vvce_connect.ui.screens.splash
 
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,11 +10,10 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.project.vvce_connect.R
-import com.project.vvce_connect.ui.theme.Purple700
+import com.project.vvce_connect.ui.screens.components.ImageComponent
 import com.project.vvce_connect.ui.theme.splash_bg
 import kotlinx.coroutines.delay
 
@@ -41,11 +39,11 @@ fun SplashScreen() {
             .background(splash_bg),
         contentAlignment = Alignment.Center
     ) {
-        Image(painter = painterResource(id = R.drawable.vvce_logo), contentDescription = "Logo")
+        ImageComponent(painter = painterResource(id = R.drawable.vvce_logo), content = "Logo")
     }
 }
 
-@Preview (showBackground = true)
+@Preview(showBackground = true)
 @Composable
 fun SplashScreenPreview() {
     SplashScreen()
