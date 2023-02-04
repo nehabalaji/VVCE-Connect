0949e44d43b9ba8f11685d69577ea494577fca7f
nehabalaji <nehabalaji10@gmail.com>
2023-02-04 14:06:21 +0530

    set up authentication module

---
+++ Authentication/src/main/java/com/example/authentication/ui/components/TextFieldComponent.kt
@@ -0,0 +1,32 @@
+package com.example.authentication.ui.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TextFieldComponent(
+    label: String,
+    modifier: Modifier = Modifier
+        .padding(horizontal = 8.dp, vertical = 4.dp)
+) {
+    var text by rememberSaveable() {
+        mutableStateOf("")
+    }
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text(label) }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextFieldComponentPreview() {
+    TextFieldComponent("Username")
+}
