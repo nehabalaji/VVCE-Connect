1f7c9b7f0a37ef953e7c4e2c33ff533a451ecba8
nehabalaji <nehabalaji10@gmail.com>
2023-02-11 20:45:31 +0530

    add import statements

--- app/src/main/java/com/project/vvce_connect/ui/screens/guest/contactUs/ContactUsScreen.kt
+++ app/src/main/java/com/project/vvce_connect/ui/screens/guest/contactUs/ContactUsScreen.kt
@@ -1,11 +1,10 @@
 package com.project.vvce_connect.ui.screens.guest.contactUs
 
-import android.media.Image
 import android.net.Uri
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
@@ -16,18 +15,22 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import com.project.vvce_connect.R
-import java.lang.reflect.Modifier
 import androidx.activity.compose.rememberLauncherForActivityResult as rememberLauncherForActivityResult
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import com.project.navigator.Screens.ForgotPasswordScreen.name
+import androidx.compose.ui.layout.ContentScale
 import com.project.vvce_connect.ui.screens.components.ButtonComponent
-import com.project.vvce_connect.ui.screens.components.ImageComponent
-import kotlin.text.Typography.section
 
 @Composable
 fun ContactUs(){
+    var name by rememberSaveable{ mutableStateOf("default name") }
+    var usn by rememberSaveable{ mutableStateOf("default usn") }
+    var section by rememberSaveable{ mutableStateOf("default section") }
+    var semester by rememberSaveable{ mutableStateOf("default semester") }
+    var department by rememberSaveable{ mutableStateOf("default department") }
+    var mentor by rememberSaveable{ mutableStateOf("default mentor") }
     Column(modifier = Modifier.verticalScroll(rememberScrollState()).padding(8.dp)) {
 
         ContactImage()
@@ -86,32 +89,29 @@ fun ContactUs(){
 }
 
 @Composable
-fun ContactImage(){
-    val imageUri = rememberSaveable{ mutableStateOf("") }
+fun ContactImage() {
+    val imageUri = rememberSaveable { mutableStateOf("") }
     val painter = rememberImagePainter(
-        if (imageUri.value.isEmpty())
-            R.drawable.user
-        else
-            imageUri.value
+        imageUri.value.ifEmpty { R.drawable.user }
     )
 
-    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent) {
-        uri: Uri? ->
-        uri?.let { imageUri.value = it.toString()}
+    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
+            uri?.let { imageUri.value = it.toString() }
     }
 
-    var name by rememberSaveable{ mutableStateOf("default name") }
-    var usn by rememberSaveable{ mutableStateOf("default usn") }
-    var section by rememberSaveable{ mutableStateOf("default section") }
-    var semester by rememberSaveable{ mutableStateOf("default semester") }
-    var department by rememberSaveable{ mutableStateOf("default department") }
-    var mentor by rememberSaveable{ mutableStateOf("default mentor") }
 
-
-    Column(modifier = Modifier.padding(8.dp).fillMaxWidth(),horizontalAlignment = Alignment.CenterHorizontally){
-        Card(shape = CircleShape,modifier = Modifier.padding(8.dp).size(100.dp) {
-            Image(painter = painter,contentDescription = null, modifier = Modifier.wrapContentSize().clickable { launcher.launch("image/*")},contentScale = ContentScale.Crop)
-            
+    Column(
+        modifier = Modifier.padding(8.dp).fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Card(shape = CircleShape, modifier = Modifier.padding(8.dp).size(100.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier.wrapContentSize().clickable { launcher.launch("image/*") },
+                contentScale = ContentScale.Crop
+            )
         }
     }
+}
 
