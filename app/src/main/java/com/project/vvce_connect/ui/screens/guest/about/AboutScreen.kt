505b381e79dfcee9421cfc703720996ae3aee3ae
manaswigowda <manaswi2606gowda@gmail.com>
2023-03-12 23:20:30 +0530

    about us screen ui

--- app/src/main/java/com/project/vvce_connect/ui/screens/guest/about/AboutScreen.kt
+++ app/src/main/java/com/project/vvce_connect/ui/screens/guest/about/AboutScreen.kt
@@ -1,4 +1,41 @@
 package com.project.vvce_connect.ui.screens.guest.about
 
-class AboutScreen {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.text.TextStyle
+import com.project.vvce_connect.ui.screens.components.ButtonComponent
+import com.project.vvce_connect.ui.screens.components.TextComponent
+import com.project.vvce_connect.ui.screens.components.TextFieldComponent
+
+@Composable
+fun AboutScreen() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        TextComponent(text = "Established in 1997, VVCE is an autonomous institute affiliated to" +
+                " Visvesvaraya Technological University (VTU), Belagavi, approved by AICTE & UGC, New Delhi and " +
+                "recognized by Govt. of Karnataka. Six UG programs are accredited by National Board of Accreditation, " +
+                "New Delhi. VVCE is also accredited by NAAC with “A” grade. VVCE offers seven UG programs, " +
+                "three PG programs and nine Research Centres for pursuing PhD programs, with a total student population" +
+                " of over 3200. The college has excellent infrastructure coupled with qualified and passionate faculty team " +
+                "to provide a perfect learning ambience. The college is under the able leadership of Dr. B. Sadashive Gowda, " +
+                "PhD (IISc) as Principal, who has rich experience in industry and education" +
+                " fields both in India and abroad.", 
+            modifier = Modifier, style = TextStyle())
+        ButtonComponent(text = "Read more")
+    }
+    }
+}
 }
