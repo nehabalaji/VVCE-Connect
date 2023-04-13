b03e74c3781c0a496706cd9c01ba9eba62644ee4
nehabalaji <nehabalaji10@gmail.com>
2023-04-14 00:37:15 +0530

    add course work and PerformanceReportScreen

--- student/src/main/java/com/project/student/ui/courseWork/CourseWorkScreen.kt
+++ student/src/main/java/com/project/student/ui/courseWork/CourseWorkScreen.kt
@@ -1,4 +1,84 @@
 package com.project.student.ui.courseWork
 
-class CourseWorkScreen {
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.project.navigator.ComposeNavigator
+import com.project.navigator.Screens
+import com.project.student.ui.components.*
+import com.project.student.ui.courseWork.viewmodel.CourseWorkViewModel
+
+@Composable
+fun CourseWorkScreen(composeNavigator: ComposeNavigator, courseWorkViewModel: CourseWorkViewModel = hiltViewModel()) {
+    val subjectList by courseWorkViewModel.subjects.collectAsState()
+    val subjectListItems by subjectList.collectAsState(initial = emptyList())
+
+    LazyColumn() {
+        item {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp),
+                shape = RoundedCornerShape(
+                    topStart = 0.dp,
+                    topEnd = 0.dp,
+                    bottomStart = 40.dp,
+                    bottomEnd = 40.dp
+                )
+            ) {
+                Text(text = "Resources")
+            }
+        }
+
+        items(subjectListItems) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        composeNavigator.navigate(Screens.StudentCourseWorkDetails.route.plus("/$it"))
+                    },
+                shape = RoundedCornerShape(
+                    10.dp
+                )
+            ) {
+                Text(text = it)
+            }
+        }
+    }
+//    ScaffoldComponent(
+//        modifier = modifier,
+//        content = {
+//            BoxInBox(
+//                modifier = modifier,
+//                text = title,
+//                subject = subject
+//            )
+//            TextComponent(text = courseWorkViewModel.subjectList.toString(), modifier = Modifier, style = TextStyle(fontSize = 20.sp))
+// //            Log.v("TAG", courseWorkViewModel.subjects.toString())
+// //            CourseWorkGrid(coursework = , modifier = modifier)
+//        },
+//        bottomBar = {
+//            BottomNavigationBar(
+//                items = DashboardNavData.getBottomNavItems(),
+//                modifier = modifier
+//            )
+//        }
+//    )
 }
+
+// @Preview
+// @Composable
+// fun CourseWorkScreenPreview() {
+//    CourseWorkScreen(modifier = Modifier, title = "Course Work", subject = "Computer Network")
+// }
--- student/src/main/java/com/project/student/ui/performanceReport/PerformanceReportScreen.kt
+++ student/src/main/java/com/project/student/ui/performanceReport/PerformanceReportScreen.kt
@@ -1,4 +1,41 @@
 package com.project.student.ui.performanceReport
 
-class PerformanceReportScreen {
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BarChart
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.project.student.ui.components.BoxInBox
+import com.project.student.ui.components.DisplayNumberBox
+import com.project.student.ui.components.IconImageComponent
+
+@Composable
+fun PerformanceReportScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        BoxInBox(modifier = Modifier, text = "Performance Report", subject = "Operating Systems")
+        Box(
+            modifier = Modifier
+                .padding(top = 36.dp, start = 24.dp, end = 24.dp)
+                .height(300.dp)
+                .align(Alignment.CenterHorizontally)
+        ) {
+            IconImageComponent(
+                modifier = Modifier.size(248.dp),
+                image = Icons.Filled.BarChart,
+                content = "Bar Chart"
+            )
+        }
+        DisplayNumberBox(modifier = Modifier, title = "CIE 1", number = "25", color = Color.LightGray)
+        DisplayNumberBox(modifier = Modifier, title = "CIE 2", number = "21", color = Color.LightGray)
+        DisplayNumberBox(modifier = Modifier, title = "CIE 3", number = "28", color = Color.LightGray)
+    }
 }
